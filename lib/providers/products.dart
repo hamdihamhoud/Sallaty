57430fa772596f9d1bbfa91a5509df60e7bbb91a
hamd <hamdihamhoud@gmail.com>
2021-04-12 23:08:00 +0300

    added dummy data

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -19,7 +19,8 @@ class ProductsProvider with ChangeNotifier {
       ],
       category: categories[1].title,
       type: categories[1].types[1].title,
-      description: '',
+                      description:
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       specs: {
         'color': 'blue',
         'size': '42 + 43',
