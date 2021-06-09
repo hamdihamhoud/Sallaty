0cd0b951db5e3b172784d88908e3d9690a0fbebc
hamd <hamdihamhoud@gmail.com>
2021-06-09 10:17:46 +0300

    hamdi

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -293,6 +293,7 @@ class ProductsProvider with ChangeNotifier {
       description: product.description,
       discountPercentage: product.discountPercentage,
       specs: product.specs,
+      
       imageUrls: [
         'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
         'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
